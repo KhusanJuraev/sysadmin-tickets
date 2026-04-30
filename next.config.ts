c90f8ad914cd2b3fa443ee4c293742ae766d6d97
= <=>
2026-04-30 15:54:25 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "bagged-gothic-rebound.ngrok-free.dev",
-    "10.32.120.228",
+    "https://sysadmin-tickets.vercel.app/"
   ],
 };
 
