f93a4d79f5e255db03257faa624c0b50918a3570
Khusan Juraev <husanjorayevv@gmail.com>
2026-04-30 16:59:27 +0500

    fix dynamic mini app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import { MiniApp } from "@/components/app/mini-app";
+import dynamic from "next/dynamic";
+
+const MiniApp = dynamic(
+    () => import("@/components/app/mini-app").then((mod) => mod.MiniApp),
+    {
+      ssr: false,
+    }
+);
 
 export default function HomePage() {
   return <MiniApp />;
